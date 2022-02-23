1284091a62ef0c2fc52a5a2fbac15b1e3512c4ce
NotlawSD <94382996+JackBWA@users.noreply.github.com>
2022-02-23 19:59:28 +0100

    Visual character selection
    Functionality WIP
    Game load tasks planificar orden al seleccionar personaje

--- Assets/Scripts/Singleplayer/Config/CharacterManager.cs
+++ Assets/Scripts/Singleplayer/Config/CharacterManager.cs
@@ -4,12 +4,67 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
+
+    public static CharacterManager singleton;
+
+    public List<GameObject> playableCharacters = new List<GameObject>();
+
+    private int index = 0;
+
     private void Awake()
     {
-        Object[] characters = Resources.LoadAll("Characters/");
-        foreach(Object o in characters)
+        #region Singleton
+        if (singleton != null)
         {
-            Instantiate(o);
+            Debug.LogWarning($"CharacterManager multiple instances.");
+            enabled = false;
+            return;
         }
+        singleton = this;
+        #endregion
+        LoadPlayableCharacters();
+    }
+
+    private void LoadPlayableCharacters()
+    {
+        Object[] characters = Resources.LoadAll("Characters/");
+        if (characters.Length > 0)
+        {
+            foreach (Object o in characters)
+            {
+                GameObject obj = (GameObject)Instantiate(o);
+                obj.SetActive(false);
+                playableCharacters.Add(obj);
+            }
+            UpdateCharacter();
+        }
+    }
+
+    private void UpdateCharacter()
+    {
+        for (int i = 0; i < playableCharacters.Count; i++)
+        {
+            if(i == index)
+            {
+                playableCharacters[i].SetActive(true);
+            } else
+            {
+                playableCharacters[i].SetActive(false);
+            }
+        }
+    }
+
+    public void PreviousCharacter()
+    {
+        index--;
+        if (index < 0) index = playableCharacters.Count - 1;
+        UpdateCharacter();
+    }
+
+    public void NextCharacter()
+    {
+        index++;
+        if (index >= playableCharacters.Count) index = 0;
+        UpdateCharacter();
     }
 }
